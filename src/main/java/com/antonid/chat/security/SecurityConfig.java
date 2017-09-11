0944fad935dbbc8a3e1ffc8e4641547c3bb9ddd2
Antonenko Ilya <antonenkoilya01@gmail.com>
2017-09-11 13:24:02 +0200

    Init commit

---
+++ src/main/java/com/antonid/chat/security/SecurityConfig.java
@@ -0,0 +1,107 @@
+package com.antonid.chat.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("com.antonid.chat.security")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private RestAuthEntryPoint restAuthenticationEntryPoint;
+
+    private MySavedRequestAwareAuthenticationSuccessHandler authenticationSuccessHandler =
+            new MySavedRequestAwareAuthenticationSuccessHandler();
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("temporary").password("temporary").roles("ADMIN")
+                .and()
+                .withUser("user").password("userPass").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/foos").authenticated()
+                .and()
+                .formLogin()
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                .and()
+                .logout();
+    }
+
+    @Bean
+    public MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler() {
+        return new MySavedRequestAwareAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public SimpleUrlAuthenticationFailureHandler myFailureHandler() {
+        return new SimpleUrlAuthenticationFailureHandler();
+    }
+
+
+    private class MySavedRequestAwareAuthenticationSuccessHandler
+            extends SimpleUrlAuthenticationSuccessHandler {
+
+        private RequestCache requestCache = new HttpSessionRequestCache();
+
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                            Authentication authentication) throws ServletException, IOException {
+
+            SavedRequest savedRequest
+                    = requestCache.getRequest(request, response);
+
+            if (savedRequest == null) {
+                clearAuthenticationAttributes(request);
+                return;
+            }
+
+            String targetUrlParam = getTargetUrlParameter();
+
+            if (isAlwaysUseDefaultTargetUrl() ||
+                    (targetUrlParam != null && StringUtils.hasText(request.getParameter(targetUrlParam)))) {
+
+                requestCache.removeRequest(request, response);
+                clearAuthenticationAttributes(request);
+                return;
+            }
+
+            clearAuthenticationAttributes(request);
+        }
+
+        public void setRequestCache(RequestCache requestCache) {
+            this.requestCache = requestCache;
+        }
+    }
+}
